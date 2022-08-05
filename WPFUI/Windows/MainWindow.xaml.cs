3059a67f01e62e3eccbe7c656f1f318918e58ff4
Karlo Bratko <karlobratko@gmail.com>
2022-08-05 10:16:42 +0200

    feat: create WPF project with initial settings and main window
    Create WPF project. Implement Settings window and skeleton of Main window.

---
+++ WPFUI/Windows/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Football.WPFUI.Windows {
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow : Window {
+    public MainWindow() {
+      InitializeComponent();
+    }
+
+    private void HomeCountrySelected(Object sender, SelectionChangedEventArgs e) {
+
+    }
+
+    private void AwayCountrySelected(Object sender, SelectionChangedEventArgs e) {
+
+    }
+
+    private void OpenSettings(Object sender, RoutedEventArgs e) {
+
+    }
+
+    private void OpenHomeDetails(Object sender, RoutedEventArgs e) {
+
+    }
+
+    private void OpenAwayDetails(Object sender, RoutedEventArgs e) {
+
+    }
+  }
+}
