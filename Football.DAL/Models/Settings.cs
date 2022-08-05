ea66c8d9c7bd1a0847f94b720bef024555d0aaf3
Karlo Bratko <karlobratko@gmail.com>
2022-08-05 10:10:15 +0200

    feat(dal): add resolution class for managing resolutions settings
    Add resolution class, with proper ToString and formatting methods for storing into file.
    
    Update settings class, replace Tuple<Int32, Int32> with Resolution class.

--- Football.DAL/Models/Settings.cs
+++ Football.DAL/Models/Settings.cs
@@ -8,15 +8,22 @@ namespace Football.DAL.Models {
 
     public Language Language { get; set; }
     public Gender Gender { get; set; }
-    public Tuple<Int32, Int32> Resolution { get; set; }
+    public Resolution Resolution { get; set; }
     public Country Country { get; set; }
 
     public static Settings DEFAULT =>
       new Settings {
         Language = Language.En,
         Gender = Gender.Male,
-        Resolution = Tuple.Create(1000, 800),
+        Resolution = Resolution.DEFAULT,
         Country = null
+      };
+
+    public static Resolution[] RESOLUTIONS =>
+      new Resolution[] {
+        new Resolution(800, 600),
+        new Resolution(1280, 720),
+        new Resolution(1920, 1080),
       };
 
     public static Settings Parse(String line) {
@@ -25,11 +32,11 @@ namespace Football.DAL.Models {
       return new Settings {
         Language = data.Length > 0 ? (Language)Enum.Parse(typeof(Language), data[0]) : DEFAULT.Language,
         Gender = data.Length > 1 ? (Gender)Enum.Parse(typeof(Gender), data[1]) : DEFAULT.Gender,
-        Resolution = data.Length > 3 ? Tuple.Create(Int32.Parse(data[2]), Int32.Parse(data[3])) : DEFAULT.Resolution,
-        Country = data.Length > 4 ? Country.Parse(data[4]) : DEFAULT.Country
+        Resolution = data.Length > 2 ? Resolution.Parse(data[2]) : Resolution.DEFAULT,
+        Country = data.Length > 3 ? Country.Parse(data[3]) : DEFAULT.Country
       };
     }
-    public String FormatForFile() => 
-      $"{Language:d}{DELIM}{Gender:d}{DELIM}{Resolution.Item1}{DELIM}{Resolution.Item2}{DELIM}{Country?.FormatForFile()}";
+    public String FormatForFile() =>
+      $"{Language:d}{DELIM}{Gender:d}{DELIM}{Resolution.FormatForFile()}{DELIM}{Country?.FormatForFile()}";
   }
 }
