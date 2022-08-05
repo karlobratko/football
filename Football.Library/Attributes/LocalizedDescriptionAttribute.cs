6aa9ae443607e30b174a759811f1d45123b19a41
Karlo Bratko <karlobratko@gmail.com>
2022-08-05 10:15:17 +0200

    feat(lib): update Language enum, implement localization
    Add Football.Repository project for managing global project resources. Add Global folder which contains localized resources for DAL and Library classes.
    
    Implement Converter and Attribute for Language enum.

---
+++ Football.Library/Attributes/LocalizedDescriptionAttribute.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel;
+using System.Resources;
+
+namespace Football.Library.Attributes {
+  public class LocalizedDescriptionAttribute : DescriptionAttribute {
+    private readonly String _resourceKey;
+    private readonly ResourceManager _resourceManager;
+
+    public LocalizedDescriptionAttribute(String resourceKey, Type resourceType) {
+      _resourceKey = resourceKey;
+      _resourceManager = new ResourceManager(resourceType);
+    }
+
+    public override String Description {
+      get {
+        String description = _resourceManager.GetString(_resourceKey);
+        return String.IsNullOrWhiteSpace(description)
+          ? $"[[{_resourceKey}]]"
+          : description;
+      }
+    }
+  }
+}
--- Football.Library/Models/Language.cs
+++ Football.Library/Models/Language.cs
@@ -1,6 +1,14 @@
-﻿namespace Football.Library.Models {
+﻿using System.ComponentModel;
+
+using Football.Library.Attributes;
+using Football.Library.Converters;
+
+namespace Football.Library.Models {
+  [TypeConverter(typeof(EnumDescriptionTypeConverter))]
   public enum Language {
+    [LocalizedDescription("En", typeof(Resources.Global.Resources))]
     En,
+    [LocalizedDescription("Hr", typeof(Resources.Global.Resources))]
     Hr
   }
 }
